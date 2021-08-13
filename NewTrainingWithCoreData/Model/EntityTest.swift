98909afbb96d92dac2ff7e3f2f848201e829072b
GuillaumeDonzeau <gdonzeau@hotmail.com>
2021-08-13 19:01:59 +0200

    Initial Commit

---
+++ NewTrainingWithCoreData/Model/EntityTest.swift
@@ -0,0 +1,14 @@
+//
+//  EntityTest.swift
+//  NewTrainingWithCoreData
+//
+//  Created by Guillaume Donzeau on 13/08/2021.
+//
+
+import Foundation
+import CoreData
+/*
+class EntityTest: NSManagedObject {
+    
+}
+*/
---
+++ NewTrainingWithCoreData/Model/EntityUsable.swift
@@ -0,0 +1,13 @@
+//
+//  EntityUsable.swift
+//  NewTrainingWithCoreData
+//
+//  Created by Guillaume Donzeau on 13/08/2021.
+//
+
+import Foundation
+
+struct EntityUsable {
+    var name: String
+    var invited:Float
+}
